a2b5e7bea5b1c9b02b9a1e715908aa03bec28db0
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-13 21:42:42 -0600

    add form fields for city and country in Form component; create countries data file

--- clime-x/src/components/Form/Form.tsx
+++ clime-x/src/components/Form/Form.tsx
@@ -1,3 +1,15 @@
 export default function Form() {
-  return <div>Form</div>;
+  return (
+    <form>
+      <div>
+        <label htmlFor="city">City:</label>
+        <input id="city" type="text" name="city" placeholder="City" />
+      </div>
+
+      <div>
+        <label htmlFor="country">Country:</label>
+        <input id="country" type="text" name="country" placeholder="country" />
+      </div>
+    </form>
+  );
 }
