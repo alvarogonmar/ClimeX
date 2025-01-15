1ad2e3a34cd455354ed57f4f33bc39829706cf86
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-14 19:14:35 -0600

    feat: retrieve appId from environment variables in fetchWeather function

--- clime-x/src/hooks/useWeather.ts
+++ clime-x/src/hooks/useWeather.ts
@@ -4,14 +4,15 @@ import { SearchType } from "../types"
 export default function useWeather() {
 
     const fetchWeather = async (search : SearchType) => {
+
+        const appId = import.meta.env.VITE_API_KEY
         try {
 
-            const appId = ""
 
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},
             ${search.country}&appid=${appId}`
 
-            const data = await axios(geoUrl) // peticion de datos tipo get (default)
+            const {data} = await axios(geoUrl) // peticion de datos tipo get (default)
             console.log(data)
         } catch (error) {
             console.log(error)
