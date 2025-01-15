fde61f7d38665aa04b912dfc6e43c60ecd0d5c2c
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-14 18:34:04 -0600

    feat: update fetchWeather function to handle API calls with error handling

--- clime-x/src/hooks/useWeather.ts
+++ clime-x/src/hooks/useWeather.ts
@@ -2,8 +2,14 @@ import axios from "axios" // Libreria para consultar apis externas
 
 export default function useWeather() {
 
-    const fetchWeather = () => {
-        console.log("Fetching")
+    const fetchWeather = async () => {
+        try {
+
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state 
+            code},{country code}&limit={limit}&appid={API key}`
+        } catch (error) {
+            console.log(error)
+        }
     }
   return {
         fetchWeather
