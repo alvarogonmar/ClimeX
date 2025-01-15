57e4803fc868ab0e3c0196e1ed6b9eb7452a7414
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-14 19:06:24 -0600

    feat: add axios call to fetch geo data in useWeather hook

--- clime-x/src/hooks/useWeather.ts
+++ clime-x/src/hooks/useWeather.ts
@@ -10,6 +10,9 @@ export default function useWeather() {
 
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},
             ${search.country}&appid=${appId}`
+
+            const data = await axios(geoUrl) // peticion de datos tipo get (default)
+            console.log(data)
         } catch (error) {
             console.log(error)
         }
