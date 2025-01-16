2411d11c65021bbd4f92bcac44b354638fa6b5d0
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-15 21:25:52 -0600

    city not found alert rendered

--- clime-x/src/App.tsx
+++ clime-x/src/App.tsx
@@ -1,11 +1,13 @@
 import styles from "./App.module.css";
+import Alert from "./components/Alert/Alert";
 import Form from "./components/Form/Form";
 import Spinner from "./components/Spinner/Spinner";
 import WeatherDetail from "./components/WeatherDetail/WeatherDetail";
 import useWeather from "./hooks/useWeather";
 
 function App() {
-  const { weather, loading, fetchWeather, hasWeatherData } = useWeather();
+  const { weather, loading, notFound, fetchWeather, hasWeatherData } =
+    useWeather();
 
   return (
     <>
@@ -15,6 +17,7 @@ function App() {
         <Form fetchWeather={fetchWeather} />
         {loading && <Spinner />}
         {hasWeatherData && <WeatherDetail weather={weather} />}
+        {notFound && <Alert>City not found</Alert>}
       </div>
     </>
   );
