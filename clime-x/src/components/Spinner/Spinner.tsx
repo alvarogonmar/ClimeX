0abcdde6b444b0b255fb7b6521bec83f874c516a
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-15 18:43:32 -0600

    feat: enhance Spinner component with cube grid animation

--- clime-x/src/components/Spinner/Spinner.tsx
+++ clime-x/src/components/Spinner/Spinner.tsx
@@ -1,3 +1,10 @@
 export default function Spinner() {
-  return <div>Spinner</div>;
+  return (
+    <div className="sk-cube-grid">
+      <div className="sk-cube sk-cube1"></div>
+      <div className="sk-cube sk-cube2"></div>
+      <div className="sk-cube sk-cube3"></div>
+      <div className="sk-cube sk-cube4"></div>
+    </div>
+  );
 }
